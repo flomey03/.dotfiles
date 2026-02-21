3568c9046a51c463dc996f7a795998d46e59cb99
Florian Meyer <meyer.florian.janis@icloud.com>
2026-02-21 12:18:30 +0100

    added all dotfiles

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.opt
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.rnu = true
+o.number = true
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ dot_latexmkrc
@@ -0,0 +1,6 @@
+$pdf_mode = 1;
+@default_files = ('main.tex');
+$pdf_previewer = 'okular %S';
+$aux_dir = 'build';
+$pdflatex = 'pdflatex -interaction=nonstopmode -shell-escape %O %S';
+
